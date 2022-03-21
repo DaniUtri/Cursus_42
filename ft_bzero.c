f21d53791093eac2a9d64404c51f4afc622830de
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-03-21 13:34:58 +0100

    ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/21 12:55:04 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/03/21 13:23:17 by dpenas-u         ###   ########.fr       */
+/*   Created: 2022/03/21 13:20:45 by dpenas-u          #+#    #+#             */
+/*   Updated: 2022/03/21 13:33:14 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <string.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-#endif
+void	ft_bzero(void *s, size_t n)
+{
+	while (n)
+		ft_memset(s, 0, n--);
+}
