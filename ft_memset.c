856b26f6483e904319b3a1693e7eac7ba3473ab5
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-03-21 13:20:24 +0100

    ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,26 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/21 12:55:04 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/03/21 13:15:58 by dpenas-u         ###   ########.fr       */
+/*   Created: 2022/03/21 13:12:55 by dpenas-u          #+#    #+#             */
+/*   Updated: 2022/03/21 13:19:26 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <string.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-#endif
+void	*ft_memset(void *b, int c, size_t len)
+{
+	while (len--)
+		((unsigned char *)b)[len] = (unsigned char)c;
+	return (b);
+}
