aaaabbe7444bdfcf134ff6f6d79f6dc4c63902e6
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-03-21 12:40:16 +0100

    all deleted

--- libft/ft_isalpha.c
+++
@@ -1,4 +0,0 @@
-int	ft_isalpha(int c)
-{
-	return ((c > 64 && c < 91) || (c > 96 && c < 123));
-}
--- libft/ft_memset.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-
-void	*ft_memset(void *s, int c, size_t n)
-{
-	while (n--)
-		((unsigned char *)s)[n] = c;
-	return (s);
-}
