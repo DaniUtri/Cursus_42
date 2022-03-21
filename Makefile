813473ba89e0bd524d3681fa2c436d1684a75aa6
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-03-21 13:12:13 +0100

    ft_strlen.c

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/21 10:49:59 by dpenas-u          #+#    #+#              #
-#    Updated: 2022/03/21 13:07:18 by dpenas-u         ###   ########.fr        #
+#    Updated: 2022/03/21 13:11:16 by dpenas-u         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,8 @@ GCC = gcc
 
 CFLAGS = -Wall -Wextra -Werror -I.
 
-SRC = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c
+SRC = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
+	  ft_strlen.c
 
 OBJ_SRC = $(SRC:.c=.o)
 
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/21 12:55:04 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/03/21 13:11:01 by dpenas-u         ###   ########.fr       */
+/*   Created: 2022/03/21 13:08:33 by dpenas-u          #+#    #+#             */
+/*   Updated: 2022/03/21 13:10:02 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <string.h>
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-size_t	ft_strlen(const char *s);
-#endif
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
