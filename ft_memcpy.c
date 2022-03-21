a74650de649df47609347eb2c684818a81469ba7
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-03-21 13:56:29 +0100

    ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/21 12:55:04 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/03/21 13:52:32 by dpenas-u         ###   ########.fr       */
+/*   Created: 2022/03/21 13:44:02 by dpenas-u          #+#    #+#             */
+/*   Updated: 2022/03/21 13:55:29 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <string.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-#endif
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	if (!dst && !src)
+		return (dst);
+	while (n--)
+		ft_memset(dst, ((unsigned char *)src)[n], n + 1);
+	return (dst);
+}
