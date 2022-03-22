f05f682bf3324b6eac6469e15c3202bfc86c6575
DaniUtri <daniel.penasutrilla98@gmail.com>
2022-03-22 18:18:14 +0100

    casa ft_memcmp.c modificacion ft_strncmp.c

--- libft.h
+++ libft.h
@@ -24,6 +24,7 @@ size_t	ft_strlen(const char *s);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
