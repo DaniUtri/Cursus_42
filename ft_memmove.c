618578093a23229d1e5e41c9d729f062c936053e
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-03-22 10:36:53 +0100

    fix ft_memset, ft_memmove, ft_bzero y ft_memcpy

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 14:00:19 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/03/21 14:41:55 by dpenas-u         ###   ########.fr       */
+/*   Updated: 2022/03/22 10:28:57 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
+	size_t	i;
+
 	if (src > dst)
 	{
-		while (len--)
-			ft_memset(dst++, ((unsigned char *)src++)[0], 1);
+		i = -1;
+		while (++i < len)
+			ft_memset(dst + i, ((unsigned char *)src + i)[0], 1);
 	}
-	else
+	else if (src < dst)
 		ft_memcpy(dst, src, len);
 	return (dst);
 }
