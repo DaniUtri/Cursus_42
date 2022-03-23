dab157e24b1a6ee04d3c2285aae132a30a252554
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-03-23 10:10:24 +0100

    fix ft_strncmp.c casa

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 09:59:31 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/03/23 09:59:33 by dpenas-u         ###   ########.fr       */
+/*   Updated: 2022/03/23 10:10:08 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (!n)
+		return (0);
 	i = 0;
 	while (s1[i] && s2[i] && i < n - 1
 		&& ((unsigned char *)s1)[i] == ((unsigned char *)s2)[i])
