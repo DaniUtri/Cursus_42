22f366a9d5314626a6d4603afc4643135b500aa0
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-03-23 10:06:25 +0100

    header casa

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 09:59:13 by dpenas-u          #+#    #+#             */
+/*   Updated: 2022/03/23 10:06:02 by dpenas-u         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
@@ -5,7 +17,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if (!ft_strlen(little) || !len)
+	if (!ft_strlen(little))
 		return ((char *)big);
 	i = -1;
 	while (++i < len && big[i])
