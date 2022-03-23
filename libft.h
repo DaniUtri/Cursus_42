144cc617858f69f14790aa1bdf355a942f76813b
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-03-23 10:56:53 +0100

    ft_strdup.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 12:55:04 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/03/23 10:18:23 by dpenas-u         ###   ########.fr       */
+/*   Updated: 2022/03/23 10:48:28 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,5 @@ char	*ft_strrchr(const char *s, int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 #endif
