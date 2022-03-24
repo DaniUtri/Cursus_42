4131cb447ab38ca765aa3e95536af935bd6a8a17
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-03-24 12:24:33 +0100

    ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,15 +6,15 @@
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 13:45:43 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/03/23 14:18:34 by dpenas-u         ###   ########.fr       */
+/*   Updated: 2022/03/24 12:24:12 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	ft_nsplits(char const *s, char c);
-static	int	ft_strmem(char **str, char const *s, int c);
-static	void	ft_strfill(char **str, char const *s, int c);
+static	void	ft_fillmem(char **str, char const *s, char c);
+static	int	ft_countw(char const *s, char c);
+static	int	ft_givemem(char **str, char const *s, char c);
 
 char	**ft_split(char const *s, char c)
 {
@@ -23,84 +23,91 @@ char	**ft_split(char const *s, char c)
 
 	if (!s)
 		return (0);
-	n_splits = ft_nsplits(s, c);
-	str = malloc(sizeof(char *) * (n_splits + 1));
+	if (!ft_strlen(s))
+	{
+		str = malloc(sizeof(char *));
+		if (!str)
+			return (0);
+		str[0] = 0;
+		return (str);
+	}
+	n_splits = ft_countw(s, c);
+	str = malloc(sizeof(char *) * n_splits);
 	if (!str)
 		return (0);
-	if (!ft_strmem(str, s, c))
+	if (!ft_givemem(str, s, c))
 		return (0);
-	ft_strfill(str, s, c);
+	ft_fillmem(str, s, c);
 	return (str);
 }
 
-static	void	ft_strfill(char **str, char const *s, int c)
+static	void	ft_fillmem(char **str, char const *s, char c)
 {
 	int	i;
+	int	n;
 	int	j;
-	int	k;
 
 	i = -1;
 	j = 0;
-	k = 0;
+	n = 0;
 	while (s[++i])
 	{
 		if (s[i] != c)
-			str[j][k++] = s[i];
-		if (k && (s[i] == c || !s[i + 1]))
+			str[n][j++] = s[i];
+		if (j && (s[i] == c || !s[i + 1]))
 		{
-			str[j++][k] = 0;
-			k = 0;
+			str[n++][j] = 0;
+			j = 0;
 		}
 	}
+	str[n] = 0;
 }
 
-static	int	ft_strmem(char **str, char const *s, int c)
+static	int	ft_givemem(char **str, char const *s, char c)
 {
 	int	i;
-	int	n_char;
+	int	n;
 	int	j;
 
 	i = -1;
-	n_char = 0;
 	j = 0;
+	n = 0;
 	while (s[++i])
 	{
 		if (s[i] != c)
-			n_char++;
-		if (n_char && (s[i] == c || !s[i + 1]))
+			n++;
+		if (n && (s[i] == c || !s[i + 1]))
 		{
-			str[j++] = malloc(sizeof(char) * (n_char + 1));
-			if (!str[j])
-			{
-				while (j > -1)
-					free(str[j--]);
+			str[j++] = malloc(sizeof(char) * (n + 1));
+			if (!str[j - 1])
 				return (0);
-			}
-			n_char = 0;
+			n = 0;
 		}
 	}
-	str[j] = 0;
+	str[j] = malloc(sizeof(char));
+	if (!str[j])
+		return (0);
 	return (1);
 }
 
-static	int	ft_nsplits(char const *s, char c)
+static	int	ft_countw(char const *s, char c)
 {
 	int	i;
+	int	n;
 	int	is_split;
-	int	n_splits;
 
 	i = -1;
 	is_split = 0;
-	n_splits = 0;
+	n = 1;
 	while (s[++i])
 	{
 		if (s[i] != c)
 			is_split = 1;
 		if (is_split && (s[i] == c || !s[i + 1]))
 		{
-			n_splits++;
+			n++;
 			is_split = 0;
 		}
 	}
-	return (n_splits);
+	return (n);
 }
