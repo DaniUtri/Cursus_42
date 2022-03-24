13fa62e84f9ebbdf79acda2c857f952d73b2e9f8
Dani <dani@iMac-de-Dani.local>
2022-03-24 10:37:15 -0700

    ft_split without leaks

--- ft_split.c
+++ ft_split.c
@@ -12,18 +12,17 @@
 
 #include "libft.h"
 
-static	void	ft_fillmem(char **str, char const *s, char c);
 static	int	ft_countw(char const *s, char c);
-static	int	ft_givemem(char **str, char const *s, char c);
+static	void	ft_aux(char **str, char const *s, char c, int n_splt);
 
 char	**ft_split(char const *s, char c)
 {
 	char	**str;
-	int		n_splits;
+	int		n_splt;
 
 	if (!s)
 		return (0);
-	if (!ft_strlen(s))
+	if (!(*s))
 	{
 		str = malloc(sizeof(char *));
 		if (!str)
@@ -31,83 +30,53 @@ char	**ft_split(char const *s, char c)
 		str[0] = 0;
 		return (str);
 	}
-	n_splits = ft_countw(s, c);
-	str = malloc(sizeof(char *) * n_splits);
+	n_splt = ft_countw(s, c);
+	str = malloc(sizeof(char *) * (n_splt + 1));
 	if (!str)
 		return (0);
-	if (!ft_givemem(str, s, c))
-		return (0);
-	ft_fillmem(str, s, c);
+	ft_aux(str, s, c, n_splt);
 	return (str);
 }
 
-static	void	ft_fillmem(char **str, char const *s, char c)
+static	void	ft_aux(char **str, char const *s, char c, int n_splt)
 {
 	int	i;
-	int	n;
-	int	j;
+	int	start;
+	int	len;
 
 	i = -1;
-	j = 0;
-	n = 0;
-	while (s[++i])
+	start = -1;
+	while (++i < n_splt)
 	{
-		if (s[i] != c)
-			str[n][j++] = s[i];
-		if (j && (s[i] == c || !s[i + 1]))
-		{
-			str[n++][j] = 0;
-			j = 0;
-		}
+		while (*s && *s == c)
+			s++;
+		if (ft_strchr(s, c))
+			len = ft_strchr(s, c) - s;
+		else
+			len = ft_strlen(s);
+		str[i] = ft_substr(s, 0, len);
+		s += len;
 	}
-	str[n] = 0;
-}
-
-static	int	ft_givemem(char **str, char const *s, char c)
-{
-	int	i;
-	int	n;
-	int	j;
-
-	i = -1;
-	j = 0;
-	n = 0;
-	while (s[++i])
-	{
-		if (s[i] != c)
-			n++;
-		if (n && (s[i] == c || !s[i + 1]))
-		{
-			str[j++] = malloc(sizeof(char) * (n + 1));
-			if (!str[j - 1])
-				return (0);
-			n = 0;
-		}
-	}
-	str[j] = malloc(sizeof(char));
-	if (!str[j])
-		return (0);
-	return (1);
+	str[n_splt] = 0;
 }
 
 static	int	ft_countw(char const *s, char c)
 {
-	int	i;
 	int	n;
 	int	is_split;
 
-	i = -1;
+	n = 0;
 	is_split = 0;
-	n = 1;
-	while (s[++i])
+	while (*s)
 	{
-		if (s[i] != c)
-			is_split = 1;
-		if (is_split && (s[i] == c || !s[i + 1]))
+		if (*s != c)
+			is_split++;
+		if (is_split && (*s == c || !*(s + 1)))
 		{
 			n++;
 			is_split = 0;
 		}
+		s++;
 	}
 	return (n);
 }
