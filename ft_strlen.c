aa8467dc751c53743aa1838869d00a9afe852387
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-04-05 15:37:46 +0200

    ft_strlen fixed not s

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 13:08:33 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/03/21 13:10:02 by dpenas-u         ###   ########.fr       */
+/*   Updated: 2022/04/05 15:34:22 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
-	while (s[i])
+	while (s && s[i])
 		i++;
 	return (i);
 }
